4a36875e029cfd0d0b5f7ba02b08518166a0756a
Mayen Kalu <kalumayen@gmail.com>
2023-04-03 20:52:24 +0100

    fixed code

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
     The size of the square must be the first argument 
     of the program.
 */
+
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
@@ -13,19 +13,11 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-const size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10)
 
-if (isNaN(size)) {
-    process.stderr.write("Invalid argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
-}
-
-for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let j = 0; j < size; j++) {
-        row += "#";
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
     }
-    console.log(row);
+    process.stdout.write("\n");
 }
