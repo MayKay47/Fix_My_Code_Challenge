b69b9a7b38443c9e1cd31484cfde41de128222c3
Mayen Kalu <kalumayen@gmail.com>
2023-06-30 12:10:09 +0100

    action: updated the status_server

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,3 +1,5 @@
+# your main server file
+
 #!/usr/bin/python3
 """
 Web server 
@@ -6,7 +8,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.errorhandler(404)
@@ -16,5 +18,5 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
+    # python -m api.v1.app
     app.run(host="0.0.0.0", port=5000)
--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-""" Index view
-"""
-from flask import jsonify
+# in api.v1.views module
 
-from api.v1.views import app_views
+from flask import Blueprint, jsonify
+
+app_views = Blueprint('app_views', __name__)
 
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
-    """ Status of the web server
-    """
     return jsonify({"status": "OK"})
