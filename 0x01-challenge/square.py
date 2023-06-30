180b8142a2fa6c784a99507d9cae610c617da53e
Mayen Kalu <kalumayen@gmail.com>
2023-06-30 09:31:56 +0100

    action: fixed square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+""" This module contains a class Square """
 
-class square():
-    
-    width = 0
-    height = 0
+class Square():
+    """ This class defines a square """
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initializes the square """
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Returns the area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def perimeter_of_my_square(self):
+        """ Returns the perimeter of the square """
+        return self.width * 4
 
     def __str__(self):
+        """ Returns the dimensions of the square """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
